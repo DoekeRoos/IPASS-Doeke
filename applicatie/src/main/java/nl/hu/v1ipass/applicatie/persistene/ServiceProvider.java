846744fbd1cf16540eb7d3ace843ed993e54f2de
DoekeRoos <doeke.roos98@gmail.com>
2017-07-18 20:34:57 +0200

    Delete ServiceProvider.java

--- applicatie/src/main/java/nl/hu/v1ipass/applicatie/persistene/ServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.v1ipass.applicatie.persistene;
-
-public class ServiceProvider {
-	private static Service service = new Service();
-	
-	public static Service getService() {
-		return service;
-	}
-}
